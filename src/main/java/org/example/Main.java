cbfcca2c8f7fe27aede938b8d38e2fd4d6c92c1a
AMBandariM <ambandarim@gmail.com>
2023-04-16 03:18:48 -0700

    doctor commit 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         //hdshdlchkdshcsdhjshdlckjsdhclsdhj
-        //nvvfdbjvbjsfdbvjhdfbskjvbhdfbvkdbvksbvjbdsh
         System.out.println(getHello());
     }
     public static String getHello() {
