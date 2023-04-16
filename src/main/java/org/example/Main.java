7763a3b8abc71f2e011a5d06ea21d8e0b77e1699
AMBandariM <ambandarim@gmail.com>
2023-04-16 03:16:59 -0700

    doctor commit 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         //hdshdlchkdshcsdhjshdlckjsdhclsdhj
+        System.out.println(getHello());
     }
     public static String getHello() {
         return "Hello, I'm a Java module!";
